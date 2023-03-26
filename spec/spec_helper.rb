3af08643df412d6cf70019ae2f598bfb3b039e93
Jonas Mueller <jonas@tigger.cloud>
2023-03-26 20:53:21 +0200

    Include sqlite3 in gems

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,13 +2,25 @@
 
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 
-require "rodauth/features/pat"
-
 require "capybara"
 require "capybara/dsl"
 require "minitest/autorun"
 
-require "sequel"
+Capybara.exact = true
+
 require "roda"
+require "sequel/core"
 require "rodauth/features/pat"
-require "rodauth/version"
+
+DB = Sequel.connect("sqlite:/", identifier_mangling: false)
+DB.extension :freeze_datasets, :date_arithmetic
+
+Base = Class.new(Roda)
+Base.plugin :flash
+Base.plugin :render, layout_opts: { path: 'spec/views/layout.str' }
+Base.plugin(:not_found) { raise "path #{request.path_info} not found" }
+
+class Base
+  attr_writer :title
+end
+
